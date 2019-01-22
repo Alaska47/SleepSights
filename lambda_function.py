5b02b7c65a8894ac815537a233c89eafb85bf6b2
Sidharth Rampally <sid.rampally@gmail.com>
2019-01-21 22:52:36 -0600

    image added

--- lambda_function.py
+++ lambda_function.py
@@ -210,3 +210,13 @@ def lambda_handler(event, context):
 		speech_text = 'You slept for '+ str(hours) +' hours and ' + str(minutes) + ' minutes last night'
 		speechlet = build_speechlet_response("sample", speech_text, "", "True")
 		return build_response({}, speechlet)
+	if (intent_name == "sleepTimings"):
+		time_slept,time_woke = get_fitbit_sleep_times(datetime.datetime.strptime(each['dateOfSleep'], "%Y-%m-%d"))
+		hours_slept, sleepData = get_fitbit_sleep_hours(date)[0]
+		print(time_slept, time_woke, sleepData)
+		#line graph of depth of sleep
+		data = [go.Bar(
+            x=['giraffes', 'orangutans', 'monkeys'],
+            y=[20, 14, 23]
+    	)]
+		py.iplot(data, filename='basic-bar')
